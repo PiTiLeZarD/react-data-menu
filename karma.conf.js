e39aaf20b8ee5644c331e4f112333dbb6bdecddf
Jonathan Adami <pitilezard@gmail.com>
2021-02-09 08:55:51 +1000

    Trying to get karma to work with parcel... not there yet

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,10 @@
-var webpack = require('webpack');
-
 module.exports = function (config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ["Chrome"],
         singleRun: true,
-        frameworks: ['mocha'],
-        files: [
-            'tests.webpack.js'
-        ],
-        preprocessors: {
-            'tests.webpack.js': ['webpack']
-        },
-        reporters: ['dots'],
-        webpack: {
-            module: {
-                loaders: [{
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader'
-                }, {
-                    test: /\.css$/,
-                    loader: "style-loader!css-loader"
-                }]
-            },
-            plugins: [
-                new webpack.DefinePlugin({
-                    'process.env': {
-                        'NODE_ENV': JSON.stringify('test')
-                    }
-                })
-            ],
-            watch: true
-        },
-        webpackServer: {
-            noInfo: true
-        }
+        frameworks: ["mocha", "parcel"],
+        plugins: ["karma-parcel"],
+        files: ["tests/*.jsx"],
+        reporters: ["dots"],
     });
 };
